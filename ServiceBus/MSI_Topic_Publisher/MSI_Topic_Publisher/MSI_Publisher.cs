e944ede48027293a35395abdc1615503383e8b9a
ishitapramanick.04 <ishitapramanick.04@outlook.com>
2024-03-25 12:11:32 +0530

    service msi

--- ServiceBus/MSI_Topic_Publisher/MSI_Topic_Publisher/MSI_Publisher.cs
+++ ServiceBus/MSI_Topic_Publisher/MSI_Topic_Publisher/MSI_Publisher.cs
@@ -5,6 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using Azure.Identity;
+using Azure.Messaging.ServiceBus;
+using System.Text;
+using System.Text.Json;
+using System;
 
 namespace MSI_Topic_Publisher
 {
@@ -13,9 +18,37 @@ namespace MSI_Topic_Publisher
         [FunctionName("MSI_Publisher")]
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
+            var queueName = "demo-publisher";
+            var messageBody = new { Id = 1, Message = "Hello, world!" }; // Example message body
 
-            log.Info("C# HTTP trigger function processed a request.");
+            // Create a managed identity credential
+            var credential = new DefaultAzureCredential();
 
+            // Create a Service Bus client
+            var client = new ServiceBusClient("servicebus_msi", credential);
+
+            // Create a sender for the queue
+            var sender = client.CreateSender(queueName);
+
+            // Create a message using the message body
+            var message = new ServiceBusMessage(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(messageBody)));
+
+            try
+            {
+                // Send the message to the queue
+                await sender.SendMessageAsync(message);
+                //log.LogInformation($"Message published to Service Bus queue '{queueName}'");
+            }
+            catch (Exception ex)
+            {
+                //log.LogError($"An error occurred while publishing message to Service Bus queue '{queueName}': {ex.Message}");
+            }
+            finally
+            {
+                // Close the sender and client
+                await sender.DisposeAsync();
+                await client.DisposeAsync();
+            }
             // parse query parameter
             string name = req.GetQueryNameValuePairs()
                 .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
