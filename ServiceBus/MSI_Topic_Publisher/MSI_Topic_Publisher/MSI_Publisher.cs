7a588a7994b96dca83e95744a2613d0908dd4c9d
ishitapramanick.04 <ishitapramanick.04@outlook.com>
2024-03-25 18:41:47 +0530

    test

--- ServiceBus/MSI_Topic_Publisher/MSI_Topic_Publisher/MSI_Publisher.cs
+++ ServiceBus/MSI_Topic_Publisher/MSI_Topic_Publisher/MSI_Publisher.cs
@@ -1,13 +1,13 @@
-using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
 using Azure.Identity;
 using Azure.Messaging.ServiceBus;
-using System.Text;
 using System.Text.Json;
 using System;
 
@@ -16,11 +16,13 @@ namespace MSI_Topic_Publisher
     public static class MSI_Publisher
     {
         [FunctionName("MSI_Publisher")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequestMessage req,
+            ILogger log)
         {
             var queueName = "demo-publisher";
-            var connectionString = "https://sb-msi-publisher.azurewebsites.net";
             var messageBody = new { Id = 1, Message = "Hello, world!" }; // Example message body
+            var connectionString = "https://sb-msi-publisher.azurewebsites.net";
 
             // Create a managed identity credential
             var credential = new DefaultAzureCredential();
@@ -37,34 +39,24 @@ namespace MSI_Topic_Publisher
             try
             {
                 // Send the message to the queue
-                await sender.SendMessageAsync(message);
-                //log.LogInformation($"Message published to Service Bus queue '{queueName}'");
+                await sender.SendMessageAsync(message).ConfigureAwait(false);
+                log.LogInformation($"Message published to Service Bus queue '{queueName}'");
             }
             catch (Exception ex)
             {
-                //log.LogError($"An error occurred while publishing message to Service Bus queue '{queueName}': {ex.Message}");
+                log.LogError($"An error occurred while publishing message to Service Bus queue '{queueName}': {ex.Message}");
+                return new StatusCodeResult((int)HttpStatusCode.InternalServerError);
             }
             finally
             {
                 // Close the sender and client
-                await sender.DisposeAsync();
+                                await sender.CloseAsync().ConfigureAwait(false);
+
                 await client.DisposeAsync();
             }
-            // parse query parameter
-            string name = req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
-                .Value;
 
-            if (name == null)
-            {
-                // Get request body
-                dynamic data = await req.Content.ReadAsAsync<object>();
-                name = data?.name;
-            }
-
-            return name == null
-                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+            return new OkObjectResult("Message published successfully");
         }
     }
 }
+
